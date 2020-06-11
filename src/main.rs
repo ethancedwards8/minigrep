6d78b9330e505ca5ebb0d8a63c8ae97a807c629f
Ethan Edwards <ethancarteredwards@gmail.com>
2020-06-10 23:50:57 -0400

    Yes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std:error:Error;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,41 +11,10 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
+    if let Err(e) = minigrep::run(config) {
+        // --snip--
+        println!("Application error: {}", e);
 
-    run(config)
-
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Erro>> {
-    let contents = fs::read_to_string(config.filename)
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+        process::exit(1);
     }
 }
-// fn parse_config(args: &[String]) -> Config {
-//     let query = args[1].clone();
-//     let filename = args[2].clone();
-
-//     Config { query, filename }
-// }
